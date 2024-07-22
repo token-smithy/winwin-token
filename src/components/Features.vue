34cd2dd2134e1c73bdfa43016a21507707fe0876
EmberSpirit007 <148816965+EmberSpirit007@users.noreply.github.com>
2024-07-22 20:28:37 +0200

    features

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,42 +1,105 @@
 <template>
-    <h2>Features</h2>
+	<h2>Features</h2>
 	<div class="features-wrapper">
-		<div class="features-left">
-			<h3 class="features__title">FREEZING (STAKING)</h3>
+		<div class="features">
+			<h3 class="features__title">TOKEN BURNING - Swap Fees</h3>
 			<div class="features__body">
 				<p>
-					Just as water turns into ice when it gets cold, $WATER tokens can be staked (iced) to earn rewards.
-					Stakers are rewarded with additional $WATER tokens over time, simulating the process of accumulating
-					ice
+					WIN-WIN scenario is challenging to achieve. Generally, the departure of participants makes win-win
+					scenarios less common. Similarly, WIN-WIN total supply is decreasing.
 				</p>
+				<p><b>Each time 100 WIN-WIN tokens are sold - total supply is decreased by 1 WIN-WIN.</b></p>
+				<card class="features-mechanism">
+					<h4>MECHANISM</h4>
+					<ul>
+						<li>Protocol is earning swap fees as a liquidity provider.</li>
+						<li>Sellers generate WIN-WIN earnings for a protocol.</li>
+						<li>Protocol collects swap fees.</li>
+						<li>All collected WIN-WIN tokens are burned by a protocol.</li>
+					</ul>
+				</card>
 			</div>
 		</div>
-		<div class="features-right">
-			<h3 class="features__title">Token burning - swap fees</h3>
+		<div class="features">
+			<h3 class="features__title">LIQUIDITY MANAGEMENT</h3>
 			<div class="features__body">
 				<p>
-					Each time 100 WIN-WIN tokens is sold into a pool, protocol earns 1 WIN-WIN token as a swap fee.
-					Protocol burns it, decreasing total supply.
+					Balancing interests is essential for sustainability of WIN-WIN scenarios, because it ensures all
+					parties benefit, fostering satisfaction, trust, collaboration, and long-term success.
 				</p>
+				<p>WIN-WIN uses liquidity management to balance holders and sellers interests.</p>
+				<card class="features-mechanism">
+					<h4>MECHANISM</h4>
+					<ul>
+						<li>Protocol at certain checkpoints is rebalancing a concentration of liquidity.</li>
+                        <li>Lower price ranges become more concentrated and higher price ranges less concentrated.</li>
+                        <li>Rebalancing concerns a minor part of the whole liquidity.</li>
+                        <li>Normal trading activity doesn't feel an effect of a rebalancing.</li>
+                        <li>Significant sell pressure (dump) does feel an effect - generating a profit for a protocol.</li>
+                        <li>Generated profit at certain checkpoints is used to make buybacks.</li>
+					</ul>
+				</card>
+			</div>
+		</div>
+		<div class="features">
+			<h3 class="features__title">BUYBACKS</h3>
+			<div class="features__body">
+				<p>
+					When sellers neglected holders interest protocol recovery plan is taking place.
+Protocol recovers holders interest by buying and burning WIN-WIN tokens.
+				</p>
+				<p>
+                    Recovering the price and decreasing total supply work in a WIN-WIN holders interest.
+
+                </p>
+				<card class="features-mechanism">
+					<h4>MECHANISM</h4>
+					<ul>
+                        <li>Profit generated from Liquidity management is used to buy tokens from a pool.</li>
+                        <li>Purchased WIN-WIN tokens are burned by a protocol.</li>
+                        <li>Assess the Damage. Understanding the full impact helps in liquidity management</li>
+                        <li>configuration.</li>
+					</ul>
+				</card>
+			</div>
+		</div>
+		<div class="features">
+			<h3 class="features__title">TOKEN NAME ON A CHART</h3>
+			<div class="features__body">
+				<p>
+					How price chart looks is one of the crucial thing for any token on a market.
+WIN-WIN has a token name drawn on a chart as a unique signature.
+Price is guaranteed to never drop below the drawing.
+
+				</p>
+				<card class="features-mechanism">
+					<h4>MECHANISM</h4>
+					<ul>
+                        <li>Drawing token name on a 5 min chart on dexscreener.</li>
+                        <li>Best community draws best following.</li>
+					</ul>
+				</card>
 			</div>
 		</div>
 	</div>
 </template>
 
+<script setup>
+import Card from "./atomaric/Card.vue";
+</script>
+
 <style lang="sass">
 .features-wrapper
-    display: flex
+    display: grid
+    grid-template-columns: 1fr 1fr
     gap: 34px
-    flex-direction: column
-    @media (min-width: 768px)
-        flex-direction: row
-    .features-left, .features-right
+    .features
         flex: 1 1 auto
         border: 1px solid rgb(64, 190, 255)
         border-radius: 14px
         max-width: 670px
         width: 100%
-        padding: 76px 29px 28px
+        padding: 56px 28px 28px 28px
         display: flex
         flex-direction: column
         align-items: center
@@ -45,4 +108,19 @@
         .features__title
             font-size: 32px
             text-align: center
+        .token-card
+            &.features-mechanism
+                padding: 22px
+                border-radius: 8px
+                border-width: 0
+                background: #0f608d
+                h4
+                    margin-bottom: 12px
+                    margin-top: 0
+                ul
+                    display: flex
+                    flex-direction: column
+                    gap: 12px
+                    margin: 0
+                    padding: 0 22px
 </style>
